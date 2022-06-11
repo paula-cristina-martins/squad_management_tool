21e634636568c5b7ba5170a820d2780c37186c1d
Paula Cristina Martins <paula172326@gmail.com>
2022-06-11 10:51:47 -0300

    feat(Tailwind): instalando lib de estilização do projeto e estruturando as pastas.

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	content: ['./src/pages/**/*.tsx', './src/components/**/*.tsx', './src/layout/**/*.tsx'],
+	theme: {
+		extend: { },
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
