d5db279676825cc40640fad9b935ac1435469436
Paula Cristina Martins <paula172326@gmail.com>
2022-06-16 18:49:08 -0300

    feat(API): criando retorno de dados ficticio para validar a estrutura.

---
+++ src/pages/api/teams.ts
@@ -0,0 +1,339 @@
+export default [
+  { 
+    id: 1, 
+    name: 'Barcelona', 
+    description: '1-Barcelona Squad', 
+    webSite: 'http://www.barcelona.com',
+    typeTeam: true,
+    totalGames: 25,
+    tags: [
+      { id: 1, description: 'BR' },
+      { id: 2, description: 'PTW' },
+      { id: 3, description: 'ATTACK' },
+      { id: 4, description: 'DEF' },
+    ], 
+    players: [
+      { 
+        id: 1, 
+        name: 'Player 01 Barcelona', 
+        age: 20, 
+        photo: 'https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=876&q=80', 
+        participated: 5
+      },
+      { 
+        id: 2, 
+        name: 'Player 02 Barcelona', 
+        age: 22, 
+        photo: 'https://images.unsplash.com/photo-1557862921-37829c790f19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80', 
+        participated: 8
+      },
+      { 
+        id: 3, 
+        name: 'Player 03 Barcelona', 
+        age: 29, 
+        photo: 'https://images.unsplash.com/photo-1581382575275-97901c2635b7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80', 
+        participated: 10
+      },
+      { 
+        id: 4, 
+        name: 'Player 04 Barcelona', 
+        age: 28, 
+        photo: 'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=465&q=80', 
+        participated: 15
+      },
+      { 
+        id: 5, 
+        name: 'Player 05 Barcelona', 
+        age: 27, 
+        photo: 'https://images.unsplash.com/photo-1581803118522-7b72a50f7e9f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80', 
+        participated: 14
+      },
+      { 
+        id: 6, 
+        name: 'Player 06 Barcelona', 
+        age: 35, 
+        photo: 'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80', 
+        participated: 23
+      },
+      { 
+        id: 7, 
+        name: 'Player 07 Barcelona', 
+        age: 30, 
+        photo: 'https://images.unsplash.com/photo-1495216875107-c6c043eb703f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80', 
+        participated: 20
+      },
+      { 
+        id: 8, 
+        name: 'Player 08 Barcelona', 
+        age: 23, 
+        photo: 'https://images.unsplash.com/photo-1610088441520-4352457e7095?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80', 
+        participated: 3
+      },
+      { 
+        id: 9, 
+        name: 'Player 09 Barcelona', 
+        age: 25, 
+        photo: 'https://images.unsplash.com/photo-1615109398623-88346a601842?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80', 
+        participated: 4
+      },
+      { 
+        id: 10, 
+        name: 'Player 10 Barcelona', 
+        age: 24, 
+        photo: 'https://images.unsplash.com/photo-1488161628813-04466f872be2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80', 
+        participated: 9
+      },
+      { 
+        id: 11, 
+        name: 'Player 11 Barcelona', 
+        age: 27, 
+        photo: 'https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80', 
+        participated: 10
+      },
+      { 
+        id: 12, 
+        name: 'Player 12 Barcelona', 
+        age: 23, 
+        photo: 'https://images.unsplash.com/photo-1583864697784-a0efc8379f70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80', 
+        participated: 20
+      },
+    ]
+  },
+  { 
+    id: 2, 
+    name: 'Real Madrid', 
+    description: '2-Real Madrid Squad', 
+    webSite: 'http://www.realMadrid.com',
+    typeTeam: true,
+    totalGames: 30,
+    tags: [
+      { id: 1, description: 'BR' },
+      { id: 4, description: 'DEF' },
+    ], 
+    players: [
+      { 
+        id: 1, 
+        name: 'Player 01 Real Madrid', 
+        age: 30, 
+        photo: 'https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=876&q=80', 
+        participated: 26
+      },
+      { 
+        id: 2, 
+        name: 'Player 02 Real Madrid', 
+        age: 32, 
+        photo: 'https://images.unsplash.com/photo-1557862921-37829c790f19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80', 
+        participated: 10
+      },
+      { 
+        id: 3, 
+        name: 'Player 03 Real Madrid', 
+        age: 29, 
+        photo: 'https://images.unsplash.com/photo-1581382575275-97901c2635b7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80', 
+        participated: 11
+      },
+      { 
+        id: 4, 
+        name: 'Player 04 Real Madrid', 
+        age: 19, 
+        photo: 'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=465&q=80', 
+        participated: 20
+      },
+      { 
+        id: 5, 
+        name: 'Player 05 Real Madrid', 
+        age: 21, 
+        photo: 'https://images.unsplash.com/photo-1581803118522-7b72a50f7e9f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80', 
+        participated: 17
+      },
+      { 
+        id: 6, 
+        name: 'Player 06 Real Madrid', 
+        age: 23, 
+        photo: 'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80', 
+        participated: 24
+      },
+      { 
+        id: 7, 
+        name: 'Player 07 Real Madrid', 
+        age: 26, 
+        photo: 'https://images.unsplash.com/photo-1495216875107-c6c043eb703f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80', 
+        participated: 25
+      },
+      { 
+        id: 8, 
+        name: 'Player 08 Real Madrid', 
+        age: 23, 
+        photo: 'https://images.unsplash.com/photo-1610088441520-4352457e7095?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80', 
+        participated: 9
+      },
+      { 
+        id: 9, 
+        name: 'Player 09 Real Madrid', 
+        age: 27, 
+        photo: 'https://images.unsplash.com/photo-1615109398623-88346a601842?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80', 
+        participated: 18
+      },
+      { 
+        id: 10, 
+        name: 'Player 10 Real Madrid', 
+        age: 24, 
+        photo: 'https://images.unsplash.com/photo-1488161628813-04466f872be2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80', 
+        participated: 15
+      },
+      { 
+        id: 11, 
+        name: 'Player 11 Real Madrid', 
+        age: 27, 
+        photo: 'https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80', 
+        participated: 13
+      },
+      { 
+        id: 12, 
+        name: 'Player 12 Real Madrid', 
+        age: 21, 
+        photo: 'https://images.unsplash.com/photo-1583864697784-a0efc8379f70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80', 
+        participated: 9
+      },
+    ]
+  },
+  { 
+    id: 3, 
+    name: 'Milan', 
+    description: '3-Milan Squad', 
+    webSite: 'http://www.milan.com',
+    typeTeam: true,
+    totalGames: 30,
+    tags: [
+      { id: 4, description: 'DEF' },
+    ], 
+    players: [],
+  },
+  { 
+    id: 4, 
+    name: 'Liverpool', 
+    description: '4-Liverpool Squad', 
+    webSite: 'http://www.liverpool.com',
+    typeTeam: true,
+    totalGames: 40,
+    tags: [
+      { id: 1, description: 'BR' },
+      { id: 2, description: 'PTW' },
+      { id: 3, description: 'ATTACK' },
+      { id: 4, description: 'DEF' },
+    ], 
+    players: [
+      { 
+        id: 1, 
+        name: 'Player 01 Liverpool', 
+        age: 25, 
+        photo: 'https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=876&q=80', 
+        participated: 21,
+      },
+      { 
+        id: 2, 
+        name: 'Player 02 Liverpool', 
+        age: 34, 
+        photo: 'https://images.unsplash.com/photo-1557862921-37829c790f19?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=871&q=80', 
+        participated: 35,
+      },
+      { 
+        id: 3, 
+        name: 'Player 03 Liverpool', 
+        age: 29, 
+        photo: 'https://images.unsplash.com/photo-1581382575275-97901c2635b7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80', 
+        participated: 29
+      },
+      { 
+        id: 4, 
+        name: 'Player 04 Liverpool', 
+        age: 26, 
+        photo: 'https://images.unsplash.com/photo-1503023345310-bd7c1de61c7d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=465&q=80', 
+        participated: 28
+      },
+      { 
+        id: 5, 
+        name: 'Player 05 Liverpool', 
+        age: 36, 
+        photo: 'https://images.unsplash.com/photo-1581803118522-7b72a50f7e9f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80', 
+        participated: 21
+      },
+      { 
+        id: 6, 
+        name: 'Player 06 Liverpool', 
+        age: 35, 
+        photo: 'https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=580&q=80', 
+        participated: 31
+      },
+      { 
+        id: 7, 
+        name: 'Player 07 Liverpool', 
+        age: 30, 
+        photo: 'https://images.unsplash.com/photo-1495216875107-c6c043eb703f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80', 
+        participated: 33
+      },
+      { 
+        id: 8, 
+        name: 'Player 08 Liverpool', 
+        age: 23, 
+        photo: 'https://images.unsplash.com/photo-1610088441520-4352457e7095?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80', 
+        participated: 34
+      },
+      { 
+        id: 9, 
+        name: 'Player 09 Liverpool', 
+        age: 25, 
+        photo: 'https://images.unsplash.com/photo-1615109398623-88346a601842?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80', 
+        participated: 31
+      },
+      { 
+        id: 10, 
+        name: 'Player 10 Liverpool', 
+        age: 26, 
+        photo: 'https://images.unsplash.com/photo-1488161628813-04466f872be2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80', 
+        participated: 23
+      },
+      { 
+        id: 11, 
+        name: 'Player 11 Liverpool', 
+        age: 27, 
+        photo: 'https://images.unsplash.com/photo-1600486913747-55e5470d6f40?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80', 
+        participated: 29
+      },
+      { 
+        id: 12, 
+        name: 'Player 12 Liverpool', 
+        age: 23, 
+        photo: 'https://images.unsplash.com/photo-1583864697784-a0efc8379f70?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80', 
+        participated: 35
+      },
+    ]
+  },
+  { 
+    id: 5,
+    name: 'Bayern Munich', 
+    description: '5-Bayern Munich Squad',
+    webSite: 'http://www.bayerMunich.com',
+    typeTeam: false,
+    totalGames: 30,
+    tags: [
+      { id: 1, description: 'BR' },
+      { id: 2, description: 'PTW' },
+      { id: 3, description: 'ATTACK' },
+      { id: 4, description: 'DEF' },
+    ], 
+    players: [],
+  },
+  { 
+    id: 6, 
+    name: 'Lazio', 
+    description: '6-Lazio Squad', 
+    webSite: 'http://www.lazio.com', 
+    typeTeam: false,
+    totalGames: 30,
+    tags: [
+      { id: 1, description: 'BR' },
+    ], 
+    players: [],
+  },
+];
